ec1315b59de2ae4c3a2006f036b022d5def019a5
VasylHafych <vasilgafich@gmail.com>
2020-04-26 14:14:17 +0200

    Updates

--- gen_data_normal-iid.jl
+++ gen_data_normal-iid.jl
@@ -18,14 +18,24 @@ using KDTree
 # Distribtions: 
 # ***************************************
 
-PATH = "Generated_Data/normal_1.jld" # path where data will be saved
+PATH = "Generated_Data/normal_4-iid.jld" # path where data will be saved
 
-f(x;) = prod(pdf.(Normal(), x))
-LogTrueIntegral(N) = 0.0
-N = 7
-min_v = -30.
-max_v = 30.
+# simple Normal Distribution: 
+
+N = 5
+min_v = -10.
+max_v = 10.
+
+dist_dim_1 = Normal(0,1)
+dist_dim = truncated(dist_dim_1, min_v, max_v)
+dist = product_distribution([dist_dim for i in 1:N])
+
+f(x::AbstractArray) = pdf(dist, x)
+
 lgV = N*log(max_v-min_v); 
+
+LogTrueIntegral(N)=0.0
+
 true_int = LogTrueIntegral(N)
 
 # ***************************************
@@ -33,13 +43,32 @@ true_int = LogTrueIntegral(N)
 algorithm = MetropolisHastings()
 log_likelihood = params -> LogDVal((log(f(params.a))))
 
-nchains = 3
+burnin = MCMCBurninStrategy(
+    max_nsamples_per_cycle = 4000,
+    max_nsteps_per_cycle = 40000,
+    max_time_per_cycle = Inf,
+    max_ncycles = 150
+)
+
+tuning = AdaptiveMetropolisTuning(
+    λ = 0.5,
+    α = 0.15..0.35,
+    β = 1.5,
+    c = 1e-4..1e2
+)
+
+AHMI_settingss = BAT.HMISettings(BAT.cholesky_partial_whitening!,
+    1000, 1.5, 0.1, true, 16, true, Dict("cov. weighted result" => BAT.hm_combineresults_covweighted!)
+)
+
+nchains = 2
 nsamples = 10^4
 max_time = 100
 max_nsteps = 10 * nsamples
 prior_bounds = [min_v, max_v] 
 
 BATPar.make_named_prior(i) = NamedTupleDist( a =  [[i[j,1]..i[j,2] for j in 1:size(i)[1]]...])
+make_distribution(i) = product_distribution([truncated(dist_dim_1, i[j,1], i[j,2]) for j in 1:size(i, 1)])
 KDTree.evaluate_total_cost(data::Data) = KDTree.cost_f_1(data)
 
 function run_integrations(cut_range::StepRange{Int64,Int64}, n_repeat::Int64; 
@@ -55,8 +84,10 @@ function run_integrations(cut_range::StepRange{Int64,Int64}, n_repeat::Int64;
     # information that we want to track 
 	integrals_ahmi_array = Vector{Float64}()
 	integrals_true_array = Vector{Float64}()
+    integrals_serial_array = Vector{Float64}()
 	cut_array = Vector{Int64}()
 	uns_ahmi_array = Vector{Float64}()
+    uns_serial_array = Vector{Float64}()
 	mcmc_time_array = Vector{Float64}()
 	n_samples_array = Vector{Int64}()
     
@@ -68,34 +99,47 @@ function run_integrations(cut_range::StepRange{Int64,Int64}, n_repeat::Int64;
         
 		for n_run in 1:n_repeat
             
-            samples, stats = bat_sample(posterior, (10^2, 20), MetropolisHastings());
+            samples  = bat_sample(dist, 600).result; 
     
-            smpl = flatview(unshaped.(samples.v))
+            smpl = convert(Array{Float64},flatview(unshaped.(samples.v)))
             weights_LogLik = samples.logd
             weights_Histogram = samples.weight;
             data_kdtree = Data(smpl[:,1:end], weights_Histogram[1:end], weights_LogLik[1:end]);
 
-            output, cost_array = DefineKDTree(data_kdtree, [1,2,3,], cut_run);
+            output, cost_array = DefineKDTree(data_kdtree, collect(1:N), cut_run);
+            
             extend_tree_bounds!(output, repeat([prior_bounds[1]], N), repeat([prior_bounds[2]], N))
 
 			@show cut_run, n_run
             
             bounds_part = extract_par_bounds(output)
 
-            algorithm = MetropolisHastings();
-
 			mcmc_ex_time = @CPUelapsed begin 
-                samples_parallel = bat_sample_parallel(log_likelihood, bounds_part, (nsamples, nchains), algorithm);
+                samples_parallel = bat_sample_parallel(make_distribution, bounds_part, nsamples*nchains, settings=AHMI_settingss);
             end
 
-			ahmi_integral_run =[sum(samples_parallel.weights_r), sqrt(sum((samples_parallel.uncertainty).^2))]
+			ahmi_integral_run =[sum(samples_parallel.integrals), sqrt(sum((samples_parallel.uncertainty).^2))] ./(cut_run+1)
+            
+            # ***
+            
+            
+            samples_serial = bat_sample(dist, (1+cut_run)*nsamples*nchains).result;
+            hmi_data = BAT.HMIData(unshaped.(samples_serial))
+            BAT.hm_integrate!(hmi_data, settings=AHMI_settingss)
+        
+            ahmi_integral_ser =[hmi_data.integralestimates["cov. weighted result"].final.estimate, hmi_data.integralestimates["cov. weighted result"].final.uncertainty]
+            log_smpl_int = ahmi_integral_ser
+            
+            # ***
             
             push!(cut_array, cut_run)
 			push!(n_samples_array, length(samples_parallel.weights_o))
 			push!(mcmc_time_array, mcmc_ex_time)
 			push!(integrals_ahmi_array, ahmi_integral_run[1])
 			push!(integrals_true_array, true_int)
+            push!(integrals_serial_array, log_smpl_int[1])
 			push!(uns_ahmi_array, ahmi_integral_run[2])
+            push!(uns_serial_array, log_smpl_int[2])
 
 		end
 		
@@ -103,8 +147,10 @@ function run_integrations(cut_range::StepRange{Int64,Int64}, n_repeat::Int64;
 		
 		save_data(deepcopy(n_samples_array), 
             deepcopy(integrals_true_array), 
-            deepcopy(integrals_ahmi_array), 
-            deepcopy(uns_ahmi_array), 
+            deepcopy(integrals_ahmi_array),
+            deepcopy(integrals_serial_array),
+            deepcopy(uns_ahmi_array),
+            deepcopy(uns_serial_array),
             deepcopy(mcmc_time_array), 
             deepcopy(cut_array), 
             deepcopy(cut_run)
@@ -116,7 +162,9 @@ end
 function save_data(n_samples_array::Vector{Int64}, 
         integrals_true_array::Vector{Float64}, 
         integrals_ahmi_array::Vector{Float64}, 
-        uns_ahmi_array::Vector{Float64}, 
+        integrals_serial_array::Vector{Float64}, 
+        uns_ahmi_array::Vector{Float64},
+        uns_serial_array::Vector{Float64},
         mcmc_time_array::Vector{Float64},  
         dim_array::Vector{Int64}, 
         dim_run::Int64, 
@@ -128,8 +176,10 @@ function save_data(n_samples_array::Vector{Int64},
     n_samples_array = reshape(n_samples_array, x_dms, y_dms)
 	integrals_true_array = reshape(integrals_true_array, x_dms, y_dms)
 	integrals_ahmi_array = reshape(integrals_ahmi_array, x_dms, y_dms)
+    integrals_serial_array = reshape(integrals_serial_array, x_dms, y_dms)
 		
 	uns_ahmi_array = reshape(uns_ahmi_array, x_dms, y_dms)
+    uns_serial_array = reshape(uns_serial_array, x_dms, y_dms)
 	mcmc_time_array = reshape(mcmc_time_array, x_dms, y_dms)
 	
     dim_array = reshape(dim_array, x_dms, y_dms)
@@ -142,8 +192,10 @@ function save_data(n_samples_array::Vector{Int64},
 	save(PATH, 
 		"sample_size", n_samples_array, 
 		"integrals_ahmi_array", integrals_ahmi_array, 
-		"integrals_true_array", integrals_true_array,  
-		"uns_ahmi_array", uns_ahmi_array,  
+		"integrals_true_array", integrals_true_array,
+        "integrals_serial_array", integrals_serial_array,
+		"uns_ahmi_array", uns_ahmi_array, 
+        "uns_serial_array", uns_serial_array, 
 		"dim_array", dim_array, 
 		"mcmc_time_array", mcmc_time_array)
 end
